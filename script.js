55ca8b088c9ec7d89c4df682020cd83210bf5c80
Mohammad Saad <midnightmelody414@gmail.com>
2025-05-09 16:25:34 +0530

    My latest changes from VS Code

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
-var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+const sounds = {
+  q: "tom-1",
+  w: "tom-2",
+  e: "tom-3",
+  r: "tom-4",
+  t: "snare",
+  y: "crash",
+  u: "kick-bass"
+};
 
-for (var i = 0; i < numberOfDrumButtons; i++) {
-  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-    var buttonInnerHTML = this.innerHTML;
-
-    makeSound(buttonInnerHTML);
-    buttonAnimation(buttonInnerHTML);
+document.querySelectorAll(".drum").forEach(button => {
+  button.addEventListener("click", function () {
+    const key = this.innerHTML;
+    makeSound(key);
+    buttonAnimation(key);
   });
-}
+});
 
 document.addEventListener("keypress", function (event) {
   makeSound(event.key);
@@ -15,54 +22,15 @@ document.addEventListener("keypress", function (event) {
 });
 
 function makeSound(key) {
-  switch (key) {
-    case "q":
-      var tom1 = new Audio("sounds/tom-1.mp3");
-      tom1.play();
-      break;
-
-    case "w":
-      var tom2 = new Audio("sounds/tom-2.mp3");
-      tom2.play();
-      break;
-
-    case "e":
-      var tom3 = new Audio("sounds/tom-3.mp3");
-      tom3.play();
-      break;
-
-    case "r":
-      var tom4 = new Audio("sounds/tom-4.mp3");
-      tom4.play();
-      break;
-
-    case "t":
-      var snare = new Audio("sounds/snare.mp3");
-      snare.play();
-      break;
-
-    case "y":
-      var crash = new Audio("sounds/crash.mp3");
-      crash.play();
-      break;
-
-    case "u":
-      var kick = new Audio("sounds/kick-bass.mp3");
-      kick.play();
-      break;
-
-    default:
-      console.log("Unmapped key pressed: " + key);
+  if (sounds[key]) {
+    new Audio("sounds/" + sounds[key] + ".mp3").play();
   }
 }
 
 function buttonAnimation(currentKey) {
-  var activeButton = document.querySelector("." + currentKey);
+  const activeButton = document.querySelector("." + currentKey);
   if (activeButton) {
     activeButton.classList.add("pressed");
-
-    setTimeout(function () {
-      activeButton.classList.remove("pressed");
-    }, 100);
+    setTimeout(() => activeButton.classList.remove("pressed"), 100);
   }
 }
